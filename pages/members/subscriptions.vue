d6de83527d4c7ea12815ad027adcca730b708fa7
Nick Hanigan <13477700+nick-wireless@users.noreply.github.com>
2024-02-22 05:00:21 +0930

    feat: draft direction for subscriptions

---
+++ pages/members/subscriptions.vue
@@ -0,0 +1,52 @@
+<script setup lang="ts">
+const { data: page } = await useAsyncData('subscriptions', () => queryContent('/members/subscriptions').findOne())
+if (!page.value) {
+  throw createError({ statusCode: 404, statusMessage: 'Page not found', fatal: true })
+}
+
+useSeoMeta({
+  title: page.value.title,
+  ogTitle: page.value.title,
+  description: page.value.description,
+  ogDescription: page.value.description
+})
+
+defineOgImage({
+  component: 'Docs',
+  title: page.value.title,
+  description: page.value.description
+})
+
+const isYearly = ref(true)
+const isSupporter = computed(() => {
+  page.value.title = 'Supporter'
+})
+</script>
+
+<template>
+  <div v-if="page">
+    <UPageHero v-bind="page.hero">
+      <!-- <template #links>
+        <UPricingToggle v-model="isYearly" class="w-48" />
+      </template> -->
+    </UPageHero>
+
+    <UContainer>
+      <UPricingGrid>
+        <UPricingCard v-for="(plan, index) in page.plans" :key="index" v-bind="plan" :price="isYearly ? plan.price.year : plan.price.month" :cycle="index!==2 ? 'once' : '/year'">
+          {{ isSupporter.value }}
+        </UPricingCard>
+      </UPricingGrid>
+    </UContainer>
+
+    <ULandingSection>
+      <ULandingLogos>
+        <UIcon v-for="icon in page.logos.icons" :key="icon" :name="icon" class="w-12 h-12 flex-shrink-0 text-gray-500 dark:text-gray-400" />
+      </ULandingLogos>
+    </ULandingSection>
+
+    <ULandingSection :title="page.faq.title" :description="page.faq.description">
+      <ULandingFAQ :items="page.faq.items" multiple default-open class="max-w-4xl mx-auto" />
+    </ULandingSection>
+  </div>
+</template>
